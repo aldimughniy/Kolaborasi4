767f80bde73f97be0cf2b9aa0582211703304605
tetynurazizah19 <tetynurazizah19.com>
2022-02-17 22:15:04 -0500

    commit

---
+++ app/src/main/java/com/example/kolaborasi4/kolaborasiActivity.java
@@ -0,0 +1,14 @@
+package com.example.kolaborasi4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class kolaborasiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kolaborasi);
+    }
+}
